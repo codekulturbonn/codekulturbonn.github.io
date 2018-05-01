3d7a5828a98d20b566d0e196ced8b8db329bd76f
Olav Schettler <olav@schettler.net>
2018-05-01 15:29:01 +0200

    URL of sources

--- src/about.mk
+++ src/about.mk
@@ -1,7 +1,7 @@
 ---
 title: Über Code &middot; Kultur &middot; Bonn
 ---
-Diese Seiten werden automatisch aus einem Verzeichnis mit Textdateien erzeugt. Die Dateien können in *Markdown* (Endung .md) oder [*Monk*](http://mulholland.xyz/docs/monk/) (Endung .mk) formatiert sein. Du möchtest einen Einträg ändern oder ergänzen? Schicke uns einfach einen [Pull Request auf Github](https://github.com/codekulturbonn/codekulturbonn.github.io).
+Diese Seiten werden automatisch aus einem [Verzeichnis mit Textdateien](https://github.com/codekulturbonn/codekulturbonn.github.io/tree/master/src) erzeugt. Die Dateien können in *Markdown* (Endung .md) oder [*Monk*](http://mulholland.xyz/docs/monk/) (Endung .mk) formatiert sein. Du möchtest einen Einträg ändern oder ergänzen? Schicke uns einfach einen [Pull Request auf Github](https://github.com/codekulturbonn/codekulturbonn.github.io).
 
 ## Autor
 
@@ -23,4 +23,4 @@ Trotz sorgfältiger inhaltlicher Kontrolle übernehme ich keine Haftung für die
 * [Ivy](http://mulholland.xyz/docs/ivy), ein in Python geschriebener Generator für statische Websites von Darren Mulholland.
 * [Doctr](https://drdoctr.github.io/doctr/) vereinfacht das Veröffentlichen der Seiten auf Github Pages mithilfe von Travis
 
-Bei jeder Änderung an den Inhalten über *git push* startet ein Skript (definiert in der Datei [.travis.yaml](https://github.com/codekulturbonn/codekulturbonn.github.io/blob/master/.travis.yml)) und lässt nacheinander Ivy und Doctr laufen, um aus den Quelldateien zuerst HTML zu erzeugen und dieses dann als [Github Pages](https://pages.github.com/) zu veröffentlichen.  
+Bei jeder Änderung an den Inhalten über *git push* startet ein Skript (definiert in der Datei [.travis.yaml](https://github.com/codekulturbonn/codekulturbonn.github.io/blob/master/.travis.yml)) und lässt nacheinander Ivy und Doctr laufen, um aus den Quelldateien zuerst HTML zu erzeugen und dieses dann als [Github Pages](https://pages.github.com/) zu veröffentlichen.
