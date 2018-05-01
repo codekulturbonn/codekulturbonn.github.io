4ae7fa860d6f8a20035c1ef4e195d148d117a0ce
Olav Schettler <olav@schettler.net>
2018-05-01 23:10:35 +0200

    ZKM

--- src/events.mk
+++ src/events.mk
@@ -3,11 +3,12 @@ title: Veranstaltungen
 ---
 ## Regelmäßige Veranstaltungen
 
-* [Girls' / Boys' Day](https://www.girls-day.de/)
 * [Hour of Code](https://code.org/)
-* Computer Science Education Week
+* [3.-9.12.2018 Computer Science Education Week](https://csedweek.org/)
 * [MakerFaire](https://makerfaire.com/)
+  * [6./7.10.2018 Mini MakerFaire Gerolstein](http://www.digitalestadtgerolstein.de/?p=7895)
 * [Barcamp Bonn](https://barcampbonn.de/)
-* [FrOSCon](https://www.froscon.de/)
-* [FutureCamp](https://futurecamp.digital/)
+* [25./26.08.2018 FrOSCon](https://www.froscon.de/)
+* [22.09.2018 FutureCamp](https://futurecamp.digital/)
 * [SocialBar](http://socialbar.de/standort/bonn/)
+* [28.03.2019 Girls' / Boys' Day](https://www.girls-day.de/)
