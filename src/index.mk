593618789bcbc7151f047c4ce90696ed7e3cd6c1
Olav Schettler <olav@schettler.net>
2018-05-01 20:43:08 +0200

    Added signature

--- src/index.mk
+++ src/index.mk
@@ -7,3 +7,7 @@ Herzlich willkommen!
 Hier listen wir zur besseren Vernetzung der Akteure Orte und Veranstaltungen in und um Bonn auf, bei denen Kinder und Jugendliche programmieren oder sonstwie kreativ mit Computern und anderen digitalen Medien sein können.
 
 Wir treffen uns auch regelmäßig. Zur Organisation der Treffen nutzen wir [eine Meetup-Gruppe](https://www.meetup.com/de-DE/Bonn-Programmieren-als-Kulturtechnik-Meetup/).
+
+Komm´ gerne vorbei!
+
+![Olav](@root/olav.png)
