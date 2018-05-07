c9bd83f03d0be8398cd6a4993bed3e9784fe35aa
Olav Schettler <olav@schettler.net>
2018-05-07 16:19:44 +0200

    Twitter feed on start page

--- src/index.mk
+++ src/index.mk
@@ -2,6 +2,9 @@
 menu title: Start
 title: Code &middot; Kultur &middot; Bonn
 ---
+:div .tweets
+  <a class="twitter-timeline" href="https://twitter.com/hashtag/codekulturbn" data-widget-id="993493483267739648">Tweets zu #codekulturbn</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
 Herzlich willkommen!
 
 Hier listen wir zur besseren Vernetzung der Akteure Orte und Veranstaltungen in und um Bonn auf, bei denen Kinder und Jugendliche programmieren oder sonstwie kreativ mit Computern und anderen digitalen Medien sein können.
