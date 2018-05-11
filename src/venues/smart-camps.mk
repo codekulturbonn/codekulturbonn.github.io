d2026c2f40a159a88639d5239aacecc73cc3c63a
Olav Schettler <olav@schettler.net>
2018-05-11 09:22:02 +0200

    New venues

---
+++ src/venues/smart-camps.mk
@@ -0,0 +1,23 @@
+---
+title: Smart Camps - BG3000 Service GmbH
+location: Bonn
+venue_url: https://www.smart-camps.de/
+---
+* Für Schulen kostenlose "Smart Camps" für Schüler
+* Unterstützt von Konrad Adenauer Stiftung und Hanns Seidel Stiftung
+* Sponsoren: Huawei, Google, Sparkassen, Bechtle
+* Bezeichnet sich selbst als _Social Impact Start Up_
+
+* Themen:
+
+	* Sichern von Technik und Daten
+  * Respektvoller Umgang im Netz
+  * Internet als Wirtschaftsraum
+  * Von passiven Konsumenten zu aktiven Mediengestaltern
+
+* Erste Veranstaltung "Digi-Camp"
+
+  * Oktober 2017
+  * im [Nicolaus-Cusanus-Gymnasium](http://www.general-anzeiger-bonn.de/bonn/bad-godesberg/Erstes-Digi-Camp-in-Bad-Godesberg-article3681646.html)
+  * Angebot für Klassen sieben bis neun
+	* Kooperation mit Barmer und TÜV
