c909229733b2c7f01bb8f8d29aa2c8d3cffe9681
Olav Schettler <olav@schettler.net>
2018-05-12 16:21:36 +0200

    Added arduino day and jugend hackt

---
+++ src/events/2018-05-12-jugend-hackt-nrw.mk
@@ -0,0 +1,12 @@
+---
+title: Jugend Hackt NRW
+location: Köln
+event_url: https://jugendhackt.org/events/koeln/
+start: 2018-05-11
+end: 2018-05-13
+---
+* [Twitter @jugendhacktNRW](https://twitter.com/jugendhacktNRW)
+* Programm zur Förderung des Programmiernachwuchses im deutschsprachigen Raum
+* Partner in Köln: [Fachstelle für Jugendmedienkultur NRW](http://www.fjmk.de/)
+* Seit 2013 von Open Knowledge Foundation Deutschland e.V. und mediale pfade.org e.V. veranstaltet
+* Bietet mit dem Programm [Hello World](https://jugendhackt.org/helloworld/hello-world-in-nordrhein-westfalen/) für Jugendzentrum oder Angebote für den offenen Ganztag
---
+++ src/venues/fjmk.mk
@@ -0,0 +1,8 @@
+---
+title: Fachstelle für Jugendmedienkultur NRW
+location: Köln
+venue_url: http://www.fjmk.de/
+---
+* Veranstaltet am 11.-13.05.2018 das [Hello World NRW](https://jugendhackt.org/helloworld/hello-world-in-nordrhein-westfalen/)
+* Kann seit 2017 Workshops für Jugendliche ab 10 Jahren in ganz NRW anbieten
+* Bietet in 2018 [zwei Praxistage](http://jugendmedienkultur-nrw.de/?p=3780) für interessierte Pädagog*innen 
