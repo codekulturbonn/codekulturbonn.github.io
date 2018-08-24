92739b954407e9c806b26c9d489e4b278049d543
Olav Schettler <olav@schettler.net>
2018-08-25 00:29:18 +0200

    Added GA article

--- src/events/2018-08-23-code-design-camp-bonn.mk
+++ src/events/2018-08-23-code-design-camp-bonn.mk
@@ -5,3 +5,4 @@ end: 2018-08-26
 fullday: true
 event_url: https://code.design/camps/bonn/1808
 ---
+[Artikel im Generalanzeiger](http://www.general-anzeiger-bonn.de/bonn/hardtberg/Nachwuchsprogrammierer-erstellen-Apps-im-Telekom-Dome-article3926914.html) vom 2018-08-23
