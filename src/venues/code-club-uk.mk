d35e7523dc8aaee0a26ffc165c7ca5f65f33703d
Olav Schettler <olav@schettler.net>
2018-08-25 09:35:45 +0200

    Added Code Club UK

---
+++ src/venues/code-club-uk.mk
@@ -0,0 +1,13 @@
+---
+title: Code Club UK
+location: >7k Clubs in UK
+venue_url: https://www.codeclubworld.org/
+---
+An enthusiastic volunteer teams up with a community venue, such as a school or library, to run an after-school club for young people aged 9-13, using specially created Code Club resources.
+
+Code Club International is a programme within the Raspberry Pi Foundation with a mission to put a Code Club in every community. 
+
+---
+Die Organisation zielt auf weltweite Verbreitung ab. Der [nächste Club von Bonn aus]() ist in Kerkrade in Limburg.
+
+Es gibt [einige Scratch-Projekte](https://codeclubprojects.org/de-DE/scratch/), die bereits ins Deutsche übersetzt wurden. Neben Scratch gibt es Projekte für _HTML_ und _Python_. 
