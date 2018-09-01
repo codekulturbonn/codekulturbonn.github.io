f706d98574c5e16a34fc805dbb5a54697990c490
Olav Schettler <olav@schettler.net>
2018-09-01 12:56:31 +0200

    Added Code4Maus

---
+++ src/events/2018-09-23-programmieren-mit-der-maus.mk
@@ -0,0 +1,7 @@
+---
+title: Thementag Digitalisierung
+start: 2018-09-23
+event_url: https://code4maus.de/
+location: Köln
+---
+* Launch von "Programmieren mit der Maus", implementiert von Freiberuflern des WDR
