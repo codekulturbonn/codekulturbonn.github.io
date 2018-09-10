bd4e8fde23e2170cd7780f0d36873c4070343790
Olav Schettler <olav@schettler.net>
2018-09-10 12:57:30 +0200

    Wissenschaft

--- src/venues/deutsches-museum-bonn.mk
+++ src/venues/deutsches-museum-bonn.mk
@@ -6,3 +6,4 @@ venue_url: http://www.deutsches-museum.de/bonn/
 * Tüfteltage, Experimentiersonntage
 * Schule im Museum: Experimentierküche, zdi-Angebote (Berufsorientierung ab der 8. Klasse), Sachunterricht-Workshops
 * Pfiffikus-Workshops (Experimente, Elektrotechnik, Technik, Detektive, Wetter, Magnetismus, Fliegen, Meer, Astronomie, Roberta, Materialien, Kosmetik, Radioaktivität)
+* Förderung durch den Verein [WISSENschaf(f)t SPASS](http://www.wissenschafft-spass.de/) - Förderverein für Bildung und Innovation im Rheinland e.V.
