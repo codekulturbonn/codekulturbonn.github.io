7538860ddfae6998dca3a71e2026927ca691660b
Olav Schettler <olav@schettler.net>
2018-09-29 08:57:52 +0200

    Meetup on 2018-10-30

--- src/index.mk
+++ src/index.mk
@@ -9,9 +9,9 @@ Herzlich willkommen!
 
 Hier listen wir zur besseren Vernetzung der Akteure Orte und Veranstaltungen in und um Bonn auf, bei denen Kinder und Jugendliche programmieren oder sonstwie kreativ mit Computern und anderen digitalen Medien sein können.
 
-Wir treffen uns regelmäßig. Zur Organisation der Treffen nutzen wir [eine Meetup-Gruppe](https://www.meetup.com/de-DE/Bonn-Programmieren-als-Kulturtechnik-Meetup/).
+Wir treffen uns regelmäßig. Das [nächste Treffen ist am 30.10. im Bonner Haus der Jugend](https://www.meetup.com/de-DE/Bonn-Programmieren-als-Kulturtechnik-Meetup/events/255050008/).
 
-Komm´ doch bei einem der nächsten Termine mal vorbei!
+Komm´ doch vorbei!
 
 :p .signature
 	![Olav](@root/olav.png)
