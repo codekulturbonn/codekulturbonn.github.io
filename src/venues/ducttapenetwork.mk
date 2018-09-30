6b0a73bf4c817aadb56d18e909044e26c119151e
Olav Schettler <olav@schettler.net>
2018-09-30 22:36:41 +0200

    Added Ducttapenetwork

---
+++ src/venues/ducttapenetwork.mk
@@ -0,0 +1,6 @@
+---
+title: Duct Tape Networks
+location: bei dir zuhause
+venue_url: http://www.ducttapenetwork.org/
+---
+Das Duct Tape Network (DTN) ist eine Reihe von spassigen, spielerischen Maker Clubs für 7-10-jährige Kinder. In den Workshops nutzen die Kinder Pappe, Klebeband, Holz, Texttilien, LED-Lampen, Motoren und mehr und erwecken damit ihre Geschichten und Erfindungen zum Leben.
