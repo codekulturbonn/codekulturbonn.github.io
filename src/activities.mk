2529fc5096fee59af337dd9f6abdb29601017f72
Olav Schettler <olav@schettler.net>
2018-09-30 21:53:52 +0200

    Mentioned RaspberryPi

--- src/activities.mk
+++ src/activities.mk
@@ -7,21 +7,31 @@ In den vergangenen Jahren habe ich ein paar Veranstaltungsformate konzipiert, di
 
 In diesem Format eines dreitägigen Workshops für Kinder von 9 bis 12 Jahren lernen die Teilnehmer Microcontroller kennen, die sich heute in fast allen elektrischen Geräten im Haushalt, im Freizeitbereich und natürlich im beruflichen Umfelld finden.
 
-Die Kinder lernen das Spiel "Der heisse Draht" kennen und bauen die Spielmaterialien aus Holzklötzen und Kupferdraht selber zusammen. Auf einer kleinen Platine löten sie dann einen sehr einfachen, aber voll funktionsfähigen Microcomputer auf Basis eines PICAXE-Microcontrollers zusammen. Wie genau die Steuerung des Spieles funktioniert, werden die Teilnehmer selber durch Programmierung des Microcontrollers festlegen. Die Programmierung erfolgt dazu in einer blockorientierten Programmierumgebung ähnlich zu bekannten Scratch. Am Ende des Workshops haben die Kinder einen ersten praktischen Einblick in die vielfältigen Einsatzmöglichkeiten von Microcontrollern gewonnen und sind vielleicht neugierig geworden, sich weiter mit diesem faszinierenden Gebiet der Technik zu beschäftigen. Die gebauten Spiele dürfen mit nach Hause genommen werden und sind ohne Computer lauffähig. Die Programmierumgebung ist kostenlos im Internet erhältlich und so kann das Spiel auch zuhause verändert oder weiterentwickelt werden.   
+Die Kinder lernen das Spiel "Der heisse Draht" kennen und bauen die Spielmaterialien aus Holzklötzen und Kupferdraht selber zusammen. Auf einer kleinen Platine löten sie dann einen sehr einfachen, aber voll funktionsfähigen Microcomputer auf Basis eines [PICAXE-Microcontrollers]() zusammen. 
+
+Wie genau die Steuerung des Spieles funktioniert, werden die Teilnehmer selber durch Programmierung des Microcontrollers festlegen. Die Programmierung erfolgt dazu in einer blockorientierten Programmierumgebung ähnlich zum bekannten Scratch. 
+
+Am Ende des Workshops haben die Kinder einen ersten praktischen Einblick in die vielfältigen Einsatzmöglichkeiten von Microcontrollern gewonnen und sind vielleicht neugierig geworden, sich weiter mit diesem faszinierenden Gebiet der Technik zu beschäftigen. Die gebauten Spiele dürfen mit nach Hause genommen werden und sind ohne Computer lauffähig. 
+
+Die Programmierumgebung ist kostenlos im Internet erhältlich und so kann das Spiel auch zuhause verändert oder weiterentwickelt werden.   
 
 Materialsammlung, Spickzettel und Beispiel-Code zum freien Herunterladen auf [Github](https://github.com/tinkerthon/Der-heisse-Draht)
 
 ## Physical Computing - Der heisse Draht (mit BBC micro:bit und Calliope Mini)
 
-Diese Variante des Workshops für 15 Schülerinnen und Schüler ab der 8. Klasse verzichtet auf das Löten und setzt auf den leistungsfähigeren BBC micro:bit bzw. seiner deutschen Cousine, der Calliope Mini auf. Auch in diesem Konzept kehren die Teilnehmer immer wieder zum Spiel "Der heisse Draht" zurück und lernen schrittweise, , wie Spielzeit und Berührungen gemessen, aufgezeichnen und ausgewertet werden. Am Ende des Workshops steht ein Wettbewerb aus drahtlos vernetzten Microcontrollern, bei dem die Ergebnisse der Teilnehmer auf einem zentralen Scoreboard angezeigt werden. 
+Diese Variante des Workshops für 15 Schülerinnen und Schüler ab der 8. Klasse verzichtet auf das Löten und setzt auf den leistungsfähigeren BBC micro:bit bzw. seiner deutschen Cousine, der Calliope Mini auf. Auch in diesem Konzept kehren die Teilnehmer immer wieder zum Spiel "Der heisse Draht" zurück und lernen schrittweise, wie Spielzeit und Berührungen gemessen, aufgezeichnet und ausgewertet werden. 
+
+Am Ende des Workshops steht ein Wettbewerb aus drahtlos vernetzten Microcontrollern, bei dem die Ergebnisse der Teilnehmer auf einem zentralen Leaderboard angezeigt werden. 
 
 Konzept, Materialsammlung und Beispiel-Code zum freien Herunterladen auf [Github](https://github.com/tinkerthon/Der-heisse-Draht-2017)
 
 ## Scratch-Nachmittag
 
-Dieses zweistündige Format eigenet sich für einen Sonntagnachmittag. Die Teilnehmer bringen ihre eigenen Maschinen mit, so dass die Veranstaltung auch gut zuhause durchgeführt werden kann. Anhand von Projektkarten realsieren die ca. 8 Teilnehmer an 9 Jahren kleine Animationsprojekte oder Spiele in der Blockprogrammiersprache Scratch.
+Dieses zweistündige Format eigenet sich für einen Sonntagnachmittag. Die Teilnehmer bringen ihre eigenen Rechner oder Tablets mit, so dass die Veranstaltung auch gut zuhause durchgeführt werden kann. Anhand von Projektkarten realisieren die ca. 8 Teilnehmer ab 9 Jahren kleine Animationsprojekte oder Spiele in der Blockprogrammiersprache Scratch.
 
 [Einführungspräsentation](https://docs.google.com/document/d/1X5QzECg7RNVS4lrvrOzDS0SXJvIM1Db3GcoIQoiRQJk/edit?usp=sharing) und [Orga-Checkliste](https://drive.google.com/open?id=1I5NfBJlMVPckFoHW3kRDsu5gBdTbIJ7D4bWTrRVCqAY) zum Herunterladen. 
+    
+*Ein Tipp:* Immer mehr Kinder haben zuhause keinen Rechner, sondern nur ein Tablet zur Verfügung. Um einfacher mit Scratch und vielleicht später mit Physical Computing oder textbasierten Programmiersprachen zu arbeiten, lohnt sich in diesem Fall die Anschaffung eines [Raspberry Pi](https://amzn.to/2OZQTOh) (Affiliate-Link zu Amazon). Zusammen mit Tastatur und Maus und einem Fernseher oder Computer-Monitor haben die Kinder einen einfachen, aber vollwertigen Computer für unter 50€.   
 
 ## Weitere Aktivitäten
 
