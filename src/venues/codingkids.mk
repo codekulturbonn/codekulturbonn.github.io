b2c746ee2f8b4a7838380138649a13e71a742e5c
Olav Schettler <olav@schettler.net>
2018-10-12 07:49:07 +0200

    Added codingkids

---
+++ src/venues/codingkids.mk
@@ -0,0 +1,6 @@
+---
+title: Coding Kids Online-Magazin
+location: online
+venue_url: http://www.codingkids.de/
+---
+Ein Online-Magazin der Einhorn Solutions GmbH aus Berlin. Tagline ist _Magazin für digitales Verstehen_.
