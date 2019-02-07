993a1d00a9a2222dd769899ff80b669f183e29a0
Olav Schettler <olav@schettler.net>
2019-02-07 20:14:19 +0100

    Added hacker school

---
+++ src/events/2019-02-16-hacker-school-bonn.mk
@@ -0,0 +1,9 @@
+---
+title: Hacker School Bonn - powered by Postbank Systems AG
+event_url: https://www.hacker-school.de/kurse/aktuell/bonn/#c76
+start: 2019-02-16
+end: 2019-02-17
+location: Bonn
+---
+* Veranstalter: Hacker School Hamburg
+* Veranstaltungsort: Postbank Systems AG, Kennedyallee 62-70, 53175 Bonn
---
+++ src/venues/hacker-school.mk
@@ -0,0 +1,8 @@
+---
+title: Hacker School
+location: Hamburg
+venue_url: https://www.hacker-school.de/
+---
+* Gegründet und durchgeführt vom Hamburger i3 e.V.
+* Veranstaltet Programmierkurse für Jugendliche zwischen 11 und 18 Jahren am Wochenende in verschiedenen Städte  
+* Die Kurse werden von ehrenamtlichen, berufstätigen Software-Entwicklern, den sogenannten *Inspirerern* geleitet
