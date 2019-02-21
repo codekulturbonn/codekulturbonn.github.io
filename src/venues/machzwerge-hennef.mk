dbbb366c9bf5b0bface7ce512ebe27d7e9cc67b0
Olav Schettler <olav@schettler.net>
2019-02-21 07:54:50 +0100

    Added Machzwerge

---
+++ src/venues/machzwerge-hennef.mk
@@ -0,0 +1,7 @@
+---
+title: Machzwerge im Machwerk e.V.
+location: Hennef
+venue_url: https://www.machwerk-hennef.de/machzwerge/
+---
+* Machzwerge sind der Nachwuchs im Machwerk
+* Machzwerge funktionieren Familienorientiert und fördern den Austausch von Wissen über die Generationen hinweg
