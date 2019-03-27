3753c67fd1f625dac19f8541d059397123b271a5
Olav Schettler <olav@schettler.net>
2019-03-27 13:08:59 +0100

    Girls day

--- src/index.mk
+++ src/index.mk
@@ -5,9 +5,13 @@ title: Code &middot; Kultur &middot; Bonn
 :div .tweets
   <a href="https://twitter.com/intent/tweet?button_hashtag=codekulturbonn&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #codekulturbonn</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-Herzlich willkommen!
+## Herzlich willkommen!
 
 Hier listen wir zur besseren Vernetzung der Akteure Orte und Veranstaltungen in und um Bonn auf, bei denen Kinder und Jugendliche programmieren oder sonstwie kreativ mit Computern und anderen digitalen Medien sein können.
+
+## 2019-03-28 Girls' & Boys' Day
+
+Den Tag verbringen wieder viele Mädchen und Jungen in Betrieben und öffentlichen Einrichtungen, um sich über interessante Berufsbilder zu informieren. [Ganz wenige Plätze sind noch frei](https://www.girls-day.de/Radar?lat=50.751&lon=7.122&zoom=11&fid=events_siegmund_freud_stra_e_25_53127_bonn&view=map&providers=events&listPage=7&free=true&where=53111%20Bonn).
 
 :p .signature
 	![Olav](@root/olav.png)
