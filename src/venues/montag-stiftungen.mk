290069ad1d6443a534eeb2da881c98cd34be1042
Olav Schettler <olav@schettler.net>
2019-04-11 16:16:54 +0200

    Added Montag-Stiftung

---
+++ src/venues/montag-stiftungen.mk
@@ -0,0 +1,11 @@
+---
+title: Montag Stiftung Jugend und Gesellschaft
+location: Bonn
+venue_url: https://www.montag-stiftungen.de/
+---
+Im Handlungsfeld _Bildung im digitalen Wandel_ arbeitet die Stiftung in vier Bereichen:
+
+* Die Montag Stiftung Jugend und Gesellschaft stärkt den Ausbau eines regionalen und – im Rahmen des [Forums Bildung Digitalisierung](https://www.montag-stiftungen.de/handlungsfelder/bildung-im-digitalen-wandel/forumbd) – bundesweiten [Netzwerkes](https://www.montag-stiftungen.de/handlungsfelder/bildung-im-digitalen-wandel/netzwerk).
+* Mit dem Projekt [Mobiler MakerSpace](https://www.montag-stiftungen.de/handlungsfelder/bildung-im-digitalen-wandel/netzwerk/mobiler-makerspace) unterstützt die Stiftung die Anwendung neuer Technik in offenen Lernformaten. Die Stiftung arbeitet mit der [Codingschule Junior](/venues/codingschule.html) und mehreren Schulen zusammen. 
+* In der [Softwareentwicklung](https://www.montag-stiftungen.de/handlungsfelder/bildung-im-digitalen-wandel/netzwerk/mobiler-makerspace) wird ein digitales Werkzeug gemeinsam mit Schulen entwickelt und erprobt.
+* Publikationen fördern den Austausch und Wissenstransfer zwischen Bildungsforschung und schulischer Praxis.
