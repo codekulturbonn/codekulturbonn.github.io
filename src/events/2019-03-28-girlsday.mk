88bc91da1be5447547286aaba48c6c9b3f406c47
Olav Schettler <olav@schettler.net>
2019-04-12 09:25:26 +0200

    No more girlsday on the start page

--- src/events/2019-03-28-girlsday.mk
+++ src/events/2019-03-28-girlsday.mk
@@ -3,3 +3,5 @@ title: Girls' / Boys' Day
 start: 2019-03-28
 event_url: https://www.girls-day.de/
 ---
+Den Tag verbringen viele Mädchen und Jungen in Betrieben und öffentlichen Einrichtungen, um sich über interessante Berufsbilder zu informieren. Kurz vor dem Termin waren [ganz wenige Plätze noch frei](https://www.girls-day.de/Radar?lat=50.751&lon=7.122&zoom=11&fid=events_siegmund_freud_stra_e_25_53127_bonn&view=map&providers=events&listPage=7&free=true&where=53111%20Bonn).
+
