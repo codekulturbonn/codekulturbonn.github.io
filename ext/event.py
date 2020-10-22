177fec99236f414dca8a57b5f19b2796d03ddf23
Olav Schettler <olav.schettler@dw.com>
2020-10-22 17:24:26 +0200

    Updates for Ivy 3

---
+++ ext/event.py
@@ -0,0 +1,13 @@
+import ivy
+
+@ivy.events.register('render_page')
+def event_sort_by_date(page):
+    event = page['node']
+    if page['is_homepage'] or len(event.path) > 1 or event.path[0] != 'events':
+        return
+
+    event['sorted_children'] = sorted(
+        event.children,
+        key=lambda e: e.meta['start'],
+        reverse=True
+    )
--- ext/venue.py
+++ ext/venue.py
@@ -5,29 +5,42 @@ import os
 @shortcodes.register('venue')
 def venue_shortcode(node, content, pargs, kwargs):
     if pargs:
-        venue = ivy.nodes.node('venues', pargs[0])
+        venue = ivy.nodes.node('@root/venues/' + pargs[0])
         if venue != None:
             return '[' + venue['title'] + '](' + venue.url + ')'
     return ''
 
-@ivy.hooks.register('file_text')
+@ivy.filters.register('file_text')
 def venue_meta_link(text, meta):
     if 'venue' in meta:
-        venue = ivy.nodes.node('venues', meta['venue'])
+        venue = ivy.nodes.node('@root/venues/' + meta['venue'])
         if venue != None:
             location = ''
             if venue['location'] != 'Bonn':
                 location += ', ' + venue['location']
-            meta['venue_link'] = '<a href="' + venue.url + '">' + venue['title'] + '</a>' + location
+            meta['venue_title'] = venue['title'] + location
+            meta['venue_link'] = '<a rel="nofollow" href="' + venue.url + '">' + venue['title'] + '</a>' + location
     return text
 
-@ivy.hooks.register('init_node')
-def venue_events(venue):
-    if len(venue.path) < 2 or venue.path[0] != 'venues':
+@ivy.events.register('render_page')
+def venue_events(page):
+    venue = page['node']
+    if page['is_homepage'] or len(venue.path) < 2 or venue.path[0] != 'venues':
         return
 
     venue['events'] = [
         event 
-            for event in ivy.nodes.node('events').childlist
+            for event in ivy.nodes.node('@root/events').children
             if 'venue' in event and event['venue'] == venue.path[1] 
     ]
+
+@ivy.events.register('render_page')
+def venue_sort_by_title(page):
+    venue = page['node']
+    if page['is_homepage'] or len(venue.path) > 1 or venue.path[0] != 'venues':
+        return
+
+    venue['sorted_children'] = sorted(
+        venue.children,
+        key=lambda v: v.meta['title']
+    )
